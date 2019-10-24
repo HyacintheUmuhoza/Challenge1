46e7aa1df230660d0b4e0046773107e703189c25
HyacintheUmuhoza <umuhozahyacinthe@akilahinstitute.org>
2019-10-24 10:49:30 +0200

    ft(Signin): Modified endpoint
    setup git
    Modified endpoint
    
    [Finishes-ft-signin-out-168982008]

--- Server/Midleware/signinValidation.js
+++ Server/Midleware/signinValidation.js
@@ -1,17 +1,17 @@
 
-// import Joi from 'joi';
+import Joi from 'joi';
 
-// const registerValidation = (req, res, next) => {
-//    const schema = {
-//        firstname: Joi.string().min(3).max(80).required(),
-//        lastname: Joi.string().min(3).max(80).required(),
-//        email: Joi.string().email().required(),
-//        password: Joi.string().alphanum().min(4).max(30).required(),
-//    };
-//    const result = Joi.validate(req.body, schema);
-//    if (result.error) {
-//        return res.status(400).json({ status: 400, error: `${result.error.details[0].message}` });
-//    }
-//    next();
-// };
-// export default registerValidation;
+const registerValidation = (req, res, next) => {
+   const schema = {
+       firstname: Joi.string().min(3).max(80).required(),
+       lastname: Joi.string().min(3).max(80).required(),
+       email: Joi.string().email().required(),
+       password: Joi.string().alphanum().min(4).max(30).required(),
+   };
+   const result = Joi.validate(req.body, schema);
+   if (result.error) {
+       return res.status(400).json({ status: 400, error: `${result.error.details[0].message}` });
+   }
+   next();
+};
+export default registerValidation;
